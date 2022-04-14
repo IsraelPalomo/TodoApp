1bf7235927791944fe4b4396eda0ac5729533722
Israel <israelpalomoe@gmail.com>
2022-04-14 14:01:29 +0200

    feat(All):Create component Groups.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="home">
     <h1 class="flex center">{{ $t('language') }}</h1>
+    <Groups />
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue';
+import Groups from '../components/Groups.vue';
 import i18n, { useI18n } from 'vue-i18n';
 import { useCounterStore } from '../store/index';
 onMounted(() => {
   useI18n().locale.value = 'es';
 });
-const counterStore = useCounterStore();
 </script>
 <style lang="scss" scoped>
 @import '../scss/main.scss';
